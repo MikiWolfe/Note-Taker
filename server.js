7aa91a7fab8e7199b6b906b583f2ba4c19cb52ed
Mikayla Bruce <mkla.w.b@gmail.com>
2022-01-20 19:42:10 -0800

    mostly works except for json files are not being added to json file correctly

--- server.js
+++ server.js
@@ -1,31 +1,54 @@
-const { json } = require('body-parser');
 const express = require('express');
-const { get } = require('http');
 // const uuid =
-const app = express()
+const path =require('path');
+const app = express();
+const fs = require('fs')
 const PORT = 3001
 
-const db = require('./db/db.json')
+const note = require('./db/db.json');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get('./notes/' , (req, res) =>{
-    res.sendFile(path.join(__dirname, '/public'))
-})
+app.get('/' , (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/'))
+);
 
-app.get('/api/notes', (req, res) =>{
+app.get('/api/notes', (req, res) => {
     res.json(`${req.method} request received to get notes`)
     console.log(`${req.method} request received to get notes`)
 })
 
 app.post('/api/notes', (req, res) => {
-console.log(`$${req.method} request received to add a note`)
+console.log(`${req.method} request received to add a note`)
 
-const { title , text } = req.body;
-if (title ){}
+const {title, text} = req.body;
+
+if (title && text){
+    const newNote = {
+        title,
+        text,
+        // review_id: uuid ()
+    };
+note.push( newNote)
+ const noteString = JSON.stringify(newNote);
+fs.appendFile('./db/db.json', noteString, (err) => err
+? console.error(err)
+:console.log(`Note for ${newNote.title} had been written to JSON file`
+)
+);
+const response = {
+    status: 'success',
+    body: newNote,
+};
+
+console.log(response);
+res.status(201).json(response)
+}else {
+    res.status(500).json('Error in posting note')
+}
 })
 
 app.listen(PORT,() =>
