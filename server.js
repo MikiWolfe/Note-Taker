8c78542d5005dd2e46c42e30305fee6efe4163c8
Mikayla Bruce <mkla.w.b@gmail.com>
2022-01-23 21:08:57 -0800

    cat added to 404 page

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const magenta= "\x1b[35m"
 
 const note = require('./db/db.json');
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/404.html'))
-})
 // Middleware:
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -18,11 +15,11 @@ app.use(express.static('public'));
 
 // GET request:
 app.get('/' , (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes' , (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
@@ -32,36 +29,36 @@ app.get('/api/notes', (req, res) => {
 
 // POST request
 app.post('/api/notes', (req, res) => {
-console.log(`${req.method} request received to add a note`)
-
-const {title, text} = req.body;
-
-if (title && text) {
-    const newNote = {
-        title,
-        text,
-        id: uuidv4()
-    };
-
-note.push(newNote);
-
-const combinedNotes = JSON.stringify(note, null, 4);
-
-fs.writeFile('./db/db.json', combinedNotes,'utf8', (err) => err
-    ? console.error(err)
-    : console.log(`Note for ${newNote.title} had been written to JSON file`));
-
-const response = {
-    status: 'success',
-    body: newNote,
-};
-
-console.log(response);
-res.status(201).json(response)
-} 
-else {
-    res.status(500).json('Error in posting note')
-}
+    console.log(`${req.method} request received to add a note`)
+    
+    const {title, text} = req.body;
+    
+    if (title && text) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        };
+        
+        note.push(newNote);
+        
+        const combinedNotes = JSON.stringify(note, null, 4);
+        
+        fs.writeFile('./db/db.json', combinedNotes,'utf8', (err) => err
+        ? console.error(err)
+        : console.log(`Note for ${newNote.title} had been written to JSON file`));
+        
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+        
+        console.log(response);
+        res.status(201).json(response)
+    } 
+    else {
+        res.status(500).json('Error in posting note')
+    }
 })
 
 // DELETE request:
@@ -74,13 +71,13 @@ app.delete('/api/notes/:id', (req, res) => {
     readFromFile('./db/db.json')
     .then((data) => JSON.parse(data))
     .then((json) => {
-
+        
         const result = json.filter((id) => id !== tipId);
-
+        
         writeToFile('./db/db.json', result);
         res.json(`Note ${tipId} has been deleted`)
     });
-  });
+});
 
 
 // `DELETE /api/notes/:id` should receive a query parameter that
@@ -90,6 +87,9 @@ app.delete('/api/notes/:id', (req, res) => {
 //  property, and then rewrite the notes to the `db.json` file.
 
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/404.html'))
+})
 
 app.listen(PORT,() =>
 console.log(magenta, `🚀 Listening at http://localhost:${PORT} 🚀`));
